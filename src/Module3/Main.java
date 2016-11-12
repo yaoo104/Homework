faacd27752de80e04948da75ee4a2a8b18439ab8
yaoo104 <aleksandryaoo@gmail.com>
2016-11-12 16:05:35 +0200

    12.11.16

--- src/Module3/Main.java
+++ src/Module3/Main.java
@@ -4,15 +4,12 @@ package Module3;
  * Created by aleks_000 on 12.11.2016.
  */
 public class Main {
-    public static void main(String[] args)
-    {
-        Bird a = new Bird("Otto");
+    public static void main(String[] args) {
+        Bird a = new Bird();
 
-        a.walk();
-        a.fly();
-        a.sing();
-        a.introduce();
-
-
+        a.sing("I am walking");
+        a.sing("I am flying");
+        a.sing("I am singing");
+        a.sing("I am Bird");
     }
 }
