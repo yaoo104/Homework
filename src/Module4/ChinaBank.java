bb077dda0cf5e87432a8610ce6b3821674b75de2
yaoo104 <aleksandryaoo@gmail.com>
2016-11-24 20:44:53 +0200

    Модуль задание 1 часть

---
+++ src/Module4/ChinaBank.java
@@ -0,0 +1,8 @@
+package Module4;
+
+/**
+ * Created by aleks_000 on 24.11.2016.
+ */
+public class ChinaBank extends Bank{
+
+}
---
+++ src/Module4/USBank.java
@@ -0,0 +1,7 @@
+package Module4;
+
+/**
+ * Created by aleks_000 on 24.11.2016.
+ */
+public class USBank extends Bank {
+}
